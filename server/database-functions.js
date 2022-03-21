d812336818f5e634f14addd9663726434b7bdd03
Vivian Trinh <vptrinh@ryerson.ca>
2022-03-21 18:56:28 -0400

    Add more database functions for adding and deleting friends, comments, and posts.

--- server/database-functions.js
+++ server/database-functions.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
@@ -20,7 +21,7 @@ const getPostsFromDog = (userid) => {
   `;
   const queryParams = [userid];
   return db.query(queryStatement, queryParams).then((data) => {
-    return Promise.resolve(data.rows[0]);
+    return Promise.resolve(data.rows);
   });
 };
 
@@ -34,8 +35,7 @@ const getFriends = (userid) => {
     return Promise.resolve(data.rows[0]);
   });
 };
-//Get posts from friends of the user
-//Need to figure out if I can use this function using getFriends and iterate through them?
+
 const getPostsFromFriends = (userid) => {
   //Shows only the posts from friends that are accepted.
   const queryStatement = `
@@ -63,6 +63,8 @@ const getUserBio = (userid) => {
   });
 };
 
+//BELOW HERE IS UNTESTED UNTIL CONNECTED TO FRONT END :)
+//Adds the content of the posts
 const addBarks = (
   dog_id,
   caption,
@@ -74,15 +76,77 @@ const addBarks = (
 ) => {
   const queryStatement = `
   INSERT INTO barks (dogs_id, caption VARCHAR(255), image_url, video_url, date_added, is_public, date_modified) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
+  const queryParams = [
+    dog_id,
+    caption,
+    image_url,
+    video_url,
+    date_added,
+    is_public,
+    date_modified,
+  ];
   return db.query(queryStatement, queryParams).then((data) => {
     return Promise.resolve(data.rows[0]);
   });
 };
 
+const deleteBarks = (bark_id) => {
+  const queryStatement = `
+  DELETE FROM barks
+  WHERE bark_id = $1;`;
+  const queryParams = [bark_id];
+  return db.query(queryStatement, queryParams).then((data) => {
+    return Promise.resolve(data.rows);
+  });
+};
+
+const postComments = (dog_id, bark_id, comment, date_added) => {
+  const queryStatement = `
+  INSERT INTO comments (dog_id, bark_id, comment, date_added) VALUES ($1, $2, $3, $4) RETURNING *;`;
+  const queryParams = [dog_id, bark_id, comment, date_added];
+  return db.query(queryStatement, queryParams).then((data) => {
+    return Promise.resolve(data.rows);
+  });
+};
+
+const deleteComments = (comment_id) => {
+  const queryStatement = `
+  DELETE FROM comments
+  WHERE comments_id = $1;`;
+  const queryParams = [comment_id];
+  return db.query(queryStatement, queryParams).then((data) => {
+    return Promise.resolve(data.rows);
+  });
+};
+
+const addFriend = (requested_dog_id, target_dog_id) => {
+  const queryStatement = `
+  INSERT INTO dog_friendlists (requested_dog_id, target_dog_id)
+  VALUES ($1, $2) RETURNING *;
+  `;
+  const queryParams = [requested_dog_id, target_dog_id];
+  return db.query(queryStatement, queryParams).then((data) => {
+    return Promise.resolve(data.rows);
+  });
+};
+
+const deleteFriend = (requested_dog_id, target_dog_id) => {
+  const queryStatement = `
+  DELETE FROM dog_friendlists
+  WHERE requested_dog_id = $1 AND target_dog_id = $2;`;
+  const queryParams = [requested_dog_id, target_dog_id];
+  return db.query(queryStatement, queryParams).then((data) => {
+    return Promise.resolve(data.rows);
+  });
+};
 module.exports = {
   getDog,
   getPostsFromDog,
   getFriends,
   getPostsFromFriends,
   addBarks,
+  deleteBarks,
+  postComments,
+  deleteComments,
+  addFriend,
 };
