fed71df413cbb4cd14a98573138f3b82428072c5
Vivian Trinh <vptrinh@ryerson.ca>
2022-03-21 20:48:32 -0400

    Update Feed using useEffect and useState

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,13 +1,55 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([
+    {
+      profilePic:
+        "https://www.nicepng.com/png/detail/1-10149_doge-deal-with-it-doge-png-transparent.png",
+      username: "Doge",
+      timestamp: "Time",
+      message: "Tibetan Mastiff",
+      image:
+        "https://images.ctfassets.net/a9237abdyvg9/5XYBjAvKsMFUid5n7Dm1hp/9d6542ace2f3e816b0498c03df8fe6ee/doge.jpeg",
+      paws: "10",
+    },
+    {
+      profilePic:
+        "https://www.nicepng.com/png/detail/1-10149_doge-deal-with-it-doge-png-transparent.png",
+      username: "Doge",
+      timestamp: "Time",
+      message: "Hey Test",
+      image:
+        "https://images.ctfassets.net/a9237abdyvg9/5XYBjAvKsMFUid5n7Dm1hp/9d6542ace2f3e816b0498c03df8fe6ee/doge.jpeg",
+      paws: "10",
+    },
+  ]);
+
+  useEffect(() => {
+    alert("OH NO ALERT");
+  }, []);
+
   return (
     <div className="feed">
       <MessageSender />
-      <Post
+      {posts.map((post) => {
+        return (
+          <Post
+            profilePic={post.profilePic}
+            username={post.username}
+            timestamp={post.timestamp}
+            message={post.message}
+            image={post.image}
+            paws={post.paws}
+          />
+        );
+      })}
+
+      {/* <Post
         profilePic="https://www.nicepng.com/png/detail/1-10149_doge-deal-with-it-doge-png-transparent.png"
         username="Doge"
         timestamp="Time"
@@ -22,7 +64,7 @@ function Feed() {
         message="Hello World"
         image="https://images.ctfassets.net/a9237abdyvg9/5XYBjAvKsMFUid5n7Dm1hp/9d6542ace2f3e816b0498c03df8fe6ee/doge.jpeg"
         paws="10"
-      />
+      /> */}
     </div>
   );
 }
