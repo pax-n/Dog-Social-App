acf458ff71b3ba4d13aa737993c684a1142396fe
Vivian Trinh <vptrinh@ryerson.ca>
2022-03-25 23:10:23 -0400

    Add friends to friend list tab.

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./FriendsList.css";
 import FriendsListUser from "./FriendsListUser";
+import axios from "axios";
+import { useContext } from "react";
+import { userContext } from "./providers/UserProvider";
 
 function FriendsList() {
   const [friends, setFriends] = useState([]);
+  const { userDog } = useContext(userContext);
+
+  useEffect(() => {
+    //Gets list of friends for user 1 and populates the friends section.
+    let dog_id = userDog;
+    axios.get(`/api/friends/${dog_id}`).then((response) => {
+      console.log("Friends response: ", response);
+      let friendlist = response.data;
+      setFriends(friendlist);
+    });
+  }, []);
+
   return (
     <div className="friendslist">
       <div className="friendslist__title">
@@ -14,8 +29,8 @@ function FriendsList() {
         {friends.map((friend) => {
           return (
             <FriendsListUser
-              profilePic={friend.profilePic}
-              user={friend.user}
+              profile_pic_url={friend.profile_pic_url}
+              name={friend.dog_name}
             />
           );
         })}
--- client/src/components/FriendsListUser.js
+++ client/src/components/FriendsListUser.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./FriendsListUser.css";
 import { Avatar } from "@mui/material";
 
-function FriendsListUser({ profilePic, user }) {
+function FriendsListUser({ profile_pic_url, name }) {
   return (
     <div className="FriendsListUser">
-      <Avatar src={profilePic} sx={{ height: "60px", width: "60px" }} />
-      <h3>{user}</h3>
+      <Avatar src={profile_pic_url} sx={{ height: "60px", width: "60px" }} />
+      <h3>{name}</h3>
     </div>
   );
 }
